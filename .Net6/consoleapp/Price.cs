768d1850ad0d6bfb996f651f5f7a87e6c0cc518e
Surender Singh <surenderssm@users.noreply.github.com>
2021-11-12 15:23:05 +0530

    Explore.net6 (#8)
    * Experiment with dotnet 6 console app
    
    * sample API

---
+++ .Net6/consoleapp/Price.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consoleapp
+{
+    public class Price
+    {
+        public void Print()
+        {
+            System.Console.WriteLine("this is form Price class.");
+        }
+    }
+}
